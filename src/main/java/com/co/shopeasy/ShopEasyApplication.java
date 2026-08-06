5638a7f7f44b7b20c8a3f6d846d887305f40413a
duvanwcuello <62266085+duvanwcuello@users.noreply.github.com>
2026-08-06 17:02:04 -0500

    Primera Carga - ClienteRepository

---
+++ src/main/java/com/co/shopeasy/ShopEasyApplication.java
@@ -0,0 +1,13 @@
+package com.co.shopeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopEasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopEasyApplication.class, args);
+    }
+
+}
