0f7158a4705cb41a48168dcd789a4353a79c94a6
E104 <duvanwcuello@gmail.com>
2026-08-11 20:54:16 -0500

    Config

---
+++ src/main/java/com/co/shopeasy/config/securityConfig.java
@@ -0,0 +1,14 @@
+package com.co.shopeasy.config;
+
+import org.apache.catalina.filters.HttpHeaderSecurityFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSecurity
+
+public class securityConfig {
+
+    @Bean
+    public SecurityFilterChain
+}
