88187fcdb79cff3a3a55adb39e03ec39267c9d3c
duvanwcuello <62266085+duvanwcuello@users.noreply.github.com>
2026-08-20 17:32:36 -0500

    Avance cliente Service 2

--- src/main/java/com/co/shopeasy/logistica/service/CamionService.java
+++ src/main/java/com/co/shopeasy/logistica/service/CamionService.java
@@ -6,6 +6,7 @@ import com.co.shopeasy.logistica.repository.CamionRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CamionService {
@@ -63,4 +64,68 @@ public class CamionService {
                 )
         );
     }
+
+    //Buscar un camion por Placa.
+    public Camion buscarPorPlaca(String placa){
+
+        if(placa==null || placa.trim().isEmpty()){
+            throw new IllegalArgumentException(
+                    "La placa es Obligatoria."
+            );
+        }
+        String placaNormalizada = placa.trim().toUpperCase();
+
+        return camionRepository.findByplaca(placaNormalizada)
+            .orElseThrow(() -> new RuntimeException(
+                    "No se encontro el camion con placa: " + placaNormalizada
+            )
+        );
+    }
+
+    //actualizar un camion
+
+    public Camion actualizarCamion(Long id, Camion  datosActializados) {
+
+        if (datosActializados == null) {
+            throw new IllegalArgumentException(
+                    "Los datos del camion son obligatorios"
+            );
+        }
+        Camion camionExistente = buscarPorId(id);
+
+        if (datosActializados.getPlaca() == null ||
+                datosActializados.getPlaca().trim().isEmpty()) {
+            throw new IllegalArgumentException(
+                    "La placa es Obligatoria."
+            );
+        }
+
+        String nuevaPlaca =
+                datosActializados.getPlaca()
+                        .trim().
+                        toUpperCase();
+
+        //Verificar nueva placa
+        Optional<Camion> camionConMismaPlaca =
+                camionRepository.findByplaca(nuevaPlaca);
+        if (camionConMismaPlaca.isPresent() && !camionConMismaPlaca.get()
+                .getId().equals(id)) {
+            throw new IllegalArgumentException(
+                    "la placa " + nuevaPlaca
+                            + " ya existe en el sistema."
+            );
+        }
+
+        camionExistente.setPlaca(nuevaPlaca);
+        camionExistente.setMarca(datosActializados.getMarca());
+        camionExistente.setModelo(datosActializados.getModelo());
+        camionExistente.setTipo(datosActializados.getTipo());
+        camionExistente.setCapacidad(
+                datosActializados.getCapacidad()
+        );
+        camionExistente.setEstado(datosActializados.getEstado());
+
+
+    }
+
 }
